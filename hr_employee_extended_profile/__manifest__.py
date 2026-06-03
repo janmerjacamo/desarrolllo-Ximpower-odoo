abb482ec058127f87ea3667edb67334cecbc50bb
janmerjacamo <160087981+janmerjacamo@users.noreply.github.com>
2026-06-03 13:05:40 -0600

    Initial commit - Employee Extended Profile

---
+++ hr_employee_extended_profile/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'HR Employee Extended Profile',
+    'version': '19.0.1.0.0',
+    'depends': ['hr'],
+    'data': [
+        'views/hr_employee_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
